81866dd230a4a7def70563a01e28a184839be53c
OlegMarmysh <56603835+OlegMarmysh@users.noreply.github.com>
2020-03-18 23:44:49 +0300

    Updates

---
+++ precache-manifest.ce67fad07d254a40d07e7cba87ae1e76.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d928d2c71f247da5803e2fabdacfd62",
+    "url": "/spring/index.html"
+  },
+  {
+    "revision": "714041e7b748f5a55c95",
+    "url": "/spring/static/css/main.2cc0d225.chunk.css"
+  },
+  {
+    "revision": "930fcbd503444ae71a0e",
+    "url": "/spring/static/js/2.3a4fb771.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/spring/static/js/2.3a4fb771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714041e7b748f5a55c95",
+    "url": "/spring/static/js/main.b3eb8ccb.chunk.js"
+  },
+  {
+    "revision": "f88d324f7f8af64d53f7",
+    "url": "/spring/static/js/runtime-main.100be23e.js"
+  },
+  {
+    "revision": "fc4350c59999bb62c468361537212419",
+    "url": "/spring/static/media/logo.fc4350c5.svg"
+  },
+  {
+    "revision": "c8a21978187d5680486ec7ad838c9c98",
+    "url": "/spring/static/media/pacman.c8a21978.svg"
+  },
+  {
+    "revision": "7f2e24fb962501672cc91ccd285ed2ba",
+    "url": "/spring/static/media/spring-boot.7f2e24fb.svg"
+  },
+  {
+    "revision": "81fe04ab129ab99da0e7c7115bb09920",
+    "url": "/spring/static/media/spring-cloud.81fe04ab.svg"
+  },
+  {
+    "revision": "79cc203ed8c54191215a60f9e5dc638f",
+    "url": "/spring/static/media/spring-data.79cc203e.svg"
+  },
+  {
+    "revision": "edf462fec682b9d48cf628eaf9e19521",
+    "url": "/spring/static/media/spring-ed.edf462fe.svg"
+  },
+  {
+    "revision": "640ad1b04f7efa89e0f0f7353e6b5e02",
+    "url": "/spring/static/media/spring-framework.640ad1b0.svg"
+  }
+]);
