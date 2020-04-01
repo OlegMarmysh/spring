730cd0b02e6dedf02f5f84eddaa4a495b03c3d71
OlegMarmysh <56603835+OlegMarmysh@users.noreply.github.com>
2020-04-01 20:14:48 +0300

    Add file netlify.toml

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/'
+  // baseURL: 'http://localhost:3001/'
+  baseURL: 'https://banana-tart-40503.herokuapp.com//'
 })
 
 export const projectsAPI = {
